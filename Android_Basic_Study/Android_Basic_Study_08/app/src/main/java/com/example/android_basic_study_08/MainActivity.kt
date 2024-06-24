4570ec2920d1af3ca3e98c578fa1fbec8f530d15
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-06-24 20:52:19 +0900

    최신 이미지 API 통해 구현

---
+++ Android_Basic_Study/Android_Basic_Study_08/app/src/main/java/com/example/android_basic_study_08/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android_basic_study_08
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.android_basic_study_08.databinding.ActivityMainBinding
+import com.example.android_basic_study_08.view.card.CardFragment
+import com.example.android_basic_study_08.view.home.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBottomNav()
+        replaceFragment(HomeFragment())
+    }
+
+    private fun setBottomNav() {
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_card -> {
+                    replaceFragment(CardFragment())
+                    return@setOnItemSelectedListener  true
+                }
+                else -> return@setOnItemSelectedListener false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
